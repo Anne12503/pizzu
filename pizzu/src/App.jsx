76242aef37215ef1ac2e244f6f68596feab29184
itsraushan99 <itsraushanraj99@gmail.com>
2024-04-22 22:45:38 +0530

    first commit

---
+++ pizzu/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { Routes,Route } from 'react-router-dom';
+import SignIn from './pages/Auth/SignIn/SignIn';
+import SignUp from './pages/Auth/SignUp/SignUp';
+import Home from './pages/Auth/Home/Home';
+
+
+function App() {
+
+  return (
+    <>
+
+      <Routes>
+        
+        <Route  path="/" element={<SignUp />} />
+        <Route  path="/SignIn" element={<SignIn />} />
+        <Route  path="/SignUp" element={<SignUp />} />
+        <Route  path="/Home" element={<Home />} />
+
+        
+
+      </Routes>
+    </>
+  )
+}
+
+export default App
